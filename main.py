088b47e17119b2e9dd68026e691a5e56e8ef7db4
Aaron Joyce <joyceaa@tcd.ie>
2025-05-10 19:02:24 +0100

    Eval repo

---
+++ main.py
@@ -0,0 +1,33 @@
+# main.py
+from fastapi import FastAPI
+from fastapi.responses import JSONResponse
+
+from ddtrace import tracer
+
+# # Initialize the tracer
+tracer.configure(
+    hostname="datadog",
+    enabled=True
+)
+
+
+app = FastAPI()
+
+@app.get("/")
+def read_root():
+    return {"Hello": "Kubernetes"}
+
+@app.get("/error")
+def read_root():
+    raise Exception("This is a bad error")
+    return {"Hello": "Kubernetes"}
+
+@app.get("/items/{item_id}")
+def read_item(item_id: int, q: str = None):
+    return {"item_id": item_id, "q": q}
+
+@app.get("/health")
+async def healthcheck():
+    return JSONResponse({
+        "ok": True
+    })
